af81e0362f51b0d4163c57757cb36d6aad8805da
gerly <gherly.ylrehg@gmail.com>
2024-10-01 21:06:51 +0700

    Js6 - Percobaan 7

--- Jobsheet6/Dosen.java
+++ Jobsheet6/Dosen.java
@@ -4,7 +4,9 @@ public class Dosen extends Pegawai {
     public String nidn;
 
     public Dosen(String nip, String nama, double gaji, String nidn) {
-        System.out.println("Objek dari class Dosen dibuat dengan constructor berparameter");
+        super(nama, nip, gaji);
+        this.nidn = nidn;
+        
     }
 
     public String getInfo() {
