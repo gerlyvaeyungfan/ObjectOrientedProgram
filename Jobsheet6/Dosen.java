8ff62d33b0f203564d18db87dc2b82ba45868181
gerly <gherly.ylrehg@gmail.com>
2024-10-01 09:45:49 +0700

    Js6 - Percobaan 4

---
+++ Jobsheet6/Dosen.java
@@ -0,0 +1,20 @@
+package Jobsheet6;
+
+public class Dosen extends Pegawai {
+    public String nidn;
+
+    public Dosen() {
+        System.out.println(gaji);
+        System.out.println("Objek dari class Dosen dibuat");
+    }
+
+    public String getAllInfo() {
+        String info = "";
+        info += "NIP    : " + super.nip + "\n";
+        info += "Nama   : " + super.nama + "\n";
+        info += "Gaji   : " + super.gaji + "\n";
+        info += "NIDN   : " + super.nidn + "\n";
+        
+        return info;
+    }
+}
