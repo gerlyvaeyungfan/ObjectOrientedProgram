016c7ff62fd0aa2ad7fd88228c643024bed8e5b8
gerly <gherly.ylrehg@gmail.com>
2024-10-03 12:11:48 +0700

    Js7 - Praktikum Latihan 2

--- Jobsheet7/Perkalianku.java
+++ Jobsheet7/Perkalianku.java
@@ -5,14 +5,14 @@ public class Perkalianku {
         System.out.println(a * b);
     }
 
-    void perkalian(int a, int b, int c) {
-        System.out.println(a * b * c);
+    void perkalian(double a, double b) {
+        System.out.println(a * b);
     }
 
     public static void main(String[] args) {
         Perkalianku objek = new Perkalianku();
 
-        objek.perkalian(24, 43);
-        objek.perkalian(34, 23, 56);
+        objek.perkalian(25, 43);
+        objek.perkalian(34.56, 23.7);
     }
 }
