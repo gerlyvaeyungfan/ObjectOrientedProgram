cd6ab92a79b3e571f350a156afb8c4fd7a87fa95
gerly <gherly.ylrehg@gmail.com>
2024-11-13 08:55:59 +0700

    JS10 - Polymorphic Arguments & instanceOf 2.5

--- Jobsheet11/Praktikum10/Demo.java
+++ Jobsheet11/Praktikum10/Demo.java
@@ -6,7 +6,7 @@ public class Demo {
     public static void main(String[] args) {
         Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom, M.Kom", "199402");
         // Dosen dosen2 = new Dosen("19700105", "Muhammad, S.T, M.T", "197001");
-        // TenagaKependidikan tendik1 = new TenagaKependidikan("19750301", " Aida, A.Md.", "Tenaga Administrasi");
+        TenagaKependidikan tendik1 = new TenagaKependidikan("19750301", " Aida, A.Md.", "Tenaga Administrasi");
         // TenagaKependidikan tendik2 = new TenagaKependidikan("19650304", "Rika, S.T.", "Tenaga Laoratorium");
 
         // ArrayList<Pegawai> daftarPegawai = new ArrayList<Pegawai>();
@@ -17,16 +17,30 @@ public class Demo {
 
         // System.out.println("Jumlah Pegawai: " + daftarPegawai.size());
 
-        Pegawai pegawai1 = dosen1;
+        // Pegawai pegawai1 = dosen1;
 
-        System.out.println(pegawai1.nip);
-        System.out.println(pegawai1.nama);
-        pegawai1.displayInfo();
+        // System.out.println(pegawai1.nip);
+        // System.out.println(pegawai1.nama);
+        // pegawai1.displayInfo();
 
-        Dosen newDosen = (Dosen) pegawai1;
+        // Dosen newDosen = (Dosen) pegawai1;
         
-        System.out.println(newDosen.nama);
-        System.out.println(newDosen.nidn);
-        newDosen.mengajar();
+        // System.out.println(newDosen.nama);
+        // System.out.println(newDosen.nidn);
+        // newDosen.mengajar();
+
+        train(dosen1);
+        train(tendik1);
+    }
+
+    public static void train(Pegawai pegawai) {
+        pegawai.displayInfo();
+        System.out.println("Mengenalkan lingkungan kampus");
+        System.out.println("Menginfokan SOP.Juknis");
+
+        if (pegawai instanceof Dosen) {
+            System.out.println("Memberikan pelatihan pedagogik");
+        }
     }
 }
+
